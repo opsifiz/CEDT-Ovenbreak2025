c5051142f2d3fad3e74680e668fc25828cd9acc5
MisterO <fifafayya@gmail.com>
2025-07-01 00:29:15 +0700

    Reformat Folder

--- Day1/X1.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-#define int long long
-using namespace std;
-
-int32_t main(){
-    int a, b, c, d, e, f, g, h;
-    cin>>a>>b>>c>>d>>e>>f>>g>>h;
-    cout<<a*b*c*d*e*f*g*h;
-}
