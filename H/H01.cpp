7e4c0a8adc8c4a17dfe9402cc429006682efece1
MisterO <fifafayya@gmail.com>
2025-07-17 02:53:02 +0700

    Update H01

--- H/H01.cpp
+++ H/H01.cpp
@@ -4,11 +4,33 @@ using namespace std;
 
 int n, x, y, k, p;
 
-int add(int a, int b){return ((a%k)+(b%k))%k;}
-
 void mains(){
     cin>>n>>x>>y>>k>>p;
-    
+    if(k == 1){
+        cout<<0<<'\n';
+        return;
+    }
+    unsigned int n2 = n*(n+1)*(n+2)/6;
+    n2 %= k;
+    int res = 0;
+    res = (res+x)%k;
+    res = (res+y)%k;
+    res = (res+((2*x*y)%k))%k;
+    res = (res*n2)%k;
+    // res = (res*n);
+    // res = (res*(n+1));
+    // res = (res*(n+2));
+    // res /= 6;
+    res %= k;
+    res = (res+n)%k;
+    int sigma_binom = 1;
+    if(n > 1) sigma_binom = 3;
+    sigma_binom *= p;
+    sigma_binom %= k;
+    res = (res+sigma_binom)%k;
+    int rd = (((n*(n+1)/2)%k)*((n*(n+1)/2)%k))%k;
+    res = (res+rd)%k;
+    cout<<res<<'\n';
 }
 
 int32_t main(){
